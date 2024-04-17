d76f1125e16803647237615f4e9060bafc280251
Izan Fuentes <ifuentes022@ikasle.ehu.eus>
2024-04-17 17:12:27 +0200

    rut atencion start sin cambio de fondos

--- source/rutinasAtencion.c
+++ source/rutinasAtencion.c
@@ -14,6 +14,15 @@ int seg3;   // Para ver si pasan tres segundos
 
 void RutAtencionTeclado ()
 {
+	if(ESTADO == JUEGO && TeclaPulsada == START){
+
+			ESTADO=PAUSA;
+			PararTempo;
+	if(ESTADO == PAUSA && TeclaPulsada == START){
+
+			ESTADO=JUEGO;
+			PonerEnMarchaTempo;
+	}
 //if (ESTADO == CERRADA)
 //{	
 //	if (TeclaPulsada()==A)
@@ -65,4 +74,3 @@ void EstablecerVectorInt()
 	irqSet(IRQ_KEYS,RutAtencionTeclado);
 	irqSet(IRQ_TIMER0,RutAtencionTempo);
 }
-
