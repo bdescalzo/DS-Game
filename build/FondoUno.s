37bc5a81e62a59500a85fa7d3051dc4bd6878eec
Urtzi Peña <upena009@ikasle.ehu.eus>
2024-04-17 15:10:10 +0200

    hehehehe

--- build/FondoUno.s
+++ build/FondoUno.s
@@ -8,7 +8,7 @@
 @	+ bitmap not compressed
 @	Total size: 98304 = 98304
 @
-@	Time-stamp: 2024-04-13, 12:17:59
+@	Time-stamp: 2024-04-15, 06:16:53
 @	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 @	( http://www.coranac.com/projects/#grit )
 @
